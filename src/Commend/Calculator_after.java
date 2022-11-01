f7f27defaeec0cc88ba3e8f76423b792cb1a5dc0
qixiaoling <xiaoling.qi@capgemini.com>
2022-11-01 17:23:28 +0100

    Commend 1

---
+++ src/Commend/Calculator_after.java
@@ -0,0 +1,4 @@
+package Commend;
+
+public class Calculator_after {
+}
---
+++ src/Commend/Calculator_before.java
@@ -0,0 +1,25 @@
+package Commend;
+
+public class Calculator_before {
+    private int value;
+
+    public Calculator_before() {
+        this.value = 0;
+    }
+
+    private void add (int valueToAdd){
+        this.value = this.value + valueToAdd;
+    }
+
+    private void subtract (int valueToSubtract) {
+        this.value = this.value - valueToSubtract;
+    }
+
+    private void multiply (int valueToMultiply) {
+        this.value = this.value * valueToMultiply;
+    }
+
+    private void divide (int valueToDivide) {
+        this.value = this.value / valueToDivide;
+    }
+}
