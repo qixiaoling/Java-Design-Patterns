b8978e8f0cd70124ad54e1fb76c866448c18b22f
qixiaoling <xiaoling.qi@capgemini.com>
2022-11-08 10:29:05 +0100

    Command 6

--- src/Command/after/Calculator_after.java
+++ src/Command/after/Calculator_after.java
@@ -5,36 +5,36 @@ import java.util.List;
 
 public class Calculator_after {
 
-    private int value = 100;
+    private int currentValue = 100;
     private List<Command> history = new ArrayList<>();
 
     public Calculator_after() {
     }
 
     public void executeCommand (Command command) {
-        this.value = command.execute(this.value);
+        this.currentValue = command.execute(this.currentValue);
         this.history.add(command);
         //record what was the last operation
     }
 
     public void executeUndo() {
         Command command = this.history.remove(history.size()-1);
-        this.value = command.undo(this.value);
+        this.currentValue = command.undo(this.currentValue);
         //the last operation command, please do an undo function of that particular command.
     }
 
     public static void main(String[] args) {
         Calculator_after calculator_after = new Calculator_after();
         calculator_after.executeCommand(new AddCommand(10));
-        System.out.println(calculator_after.value);
+        System.out.println(calculator_after.currentValue);
         calculator_after.executeUndo();
-        System.out.println(calculator_after.value);
+        System.out.println(calculator_after.currentValue);
 
         Calculator_after calculator_after_chained = new Calculator_after();
         calculator_after_chained.executeCommand(new AddThenMultiplyCommand(1, 2));
-        System.out.println(calculator_after_chained.value);
+        System.out.println(calculator_after_chained.currentValue);
         calculator_after_chained.executeUndo();
-        System.out.println(calculator_after_chained.value);
+        System.out.println(calculator_after_chained.currentValue);
     }
 
 
